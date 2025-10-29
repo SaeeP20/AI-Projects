381023287907a24268982de8ce70ca67323ae9dc
Saee <pujari.saee@gmail.com>
2025-10-29 14:42:37 +0000

    Added comments

--- emojiWeather/weather.js
+++ emojiWeather/weather.js
@@ -1,8 +1,8 @@
-import readline from "readline";
+import readline from "readline"; //imports function to read user input
 
 const API_KEY = "e3d057b74049af97b57e2c9e2c0b37ea"
 
-const emojiMap = {
+const emojiMap = { //maps the weather to emojis
     Clear: "☀️",
     Clouds: "☁️",
     Rain: "🌧️",
@@ -17,20 +17,20 @@ async function getWeather(city) {
 
     try {
         const res = await fetch(url);
-        const data = await res.json();
+        const data = await res.json(); //response handling
 
-        if (data.cod != 200) {
+        if (data.cod != 200) { //only successful if .cod returns 200 otherwise error
             console.log("Error: ", data.message);
             return;
         }
 
-        let condition = data.weather[0].main;
+        let condition = data.weather[0].main; //extracts main weather condition
         let temp = data.main.temp;
 
         let emoji = emojiMap[condition || "🤷"];
         let tempEmoji = temp > 30 ? "🔥" : temp > 20 ? "😎" : temp < 10 ? "🥶" : "🙂";
 
-        console.log(`${emoji} ${tempEmoji} ${temp}°C in ${data.name}`);
+        console.log(`${emoji} ${tempEmoji} ${temp}°C in ${data.name}`); //prints the message
     } catch (err) {
         console.error("Error fetching weather: ", err);
     }
